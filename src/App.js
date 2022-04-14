1b3e86e582657b981751a9f486af676cb8d3b030
Saadat123456 <saadatlucky313@gmail.com>
2022-04-14 10:50:33 +0500

    Add Routing and Dispatch Fetching Action

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { getStockData } from './redux/StockData/stock_data';
 import './App.css';
+import Header from './components/common/header';
+import NotFound from './components/common/not-found';
+import Details from './components/details/details';
+import Home from './components/home/home';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getStockData());
+  }, []);
   return (
-    <>
-    </>
+    <Router>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/details/:id" element={<Details />} />
+        <Route path="*" exact element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
