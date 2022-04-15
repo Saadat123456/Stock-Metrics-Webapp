09c230e28973337e0a225a940741f1badd97a169
Saadat123456 <saadatlucky313@gmail.com>
2022-04-15 10:22:48 +0500

    Refactor to remove mock data

--- src/redux/StockData/stock_data.js
+++ src/redux/StockData/stock_data.js
@@ -1,26 +1,16 @@
-// import { getCoinMarketApiUrl } from '../../strings';
-import mockData from './mock_data';
+import { getCoinMarketApiUrl } from '../../strings';
 
 const GET_STOCK_DATA = 'Stock_Metrics_Webapp/stock_data/GET';
 
 const initialState = [];
 
 export const getStockData = () => async (dispatch) => {
-  // const response = await fetch(getCoinMarketApiUrl());
-  // const data = await response.json();
-  setTimeout(() => {
-    dispatch({
-      type: GET_STOCK_DATA,
-      // data: data.data.cryptoCurrencyList,
-      data: mockData,
-    });
-  }, 2000);
-  // const data = [];
-  // dispatch({
-  //   type: GET_STOCK_DATA,
-  //   data: data.data.cryptoCurrencyList,
-  //   // data: mockData,
-  // });
+  const response = await fetch(getCoinMarketApiUrl());
+  const data = await response.json();
+  dispatch({
+    type: GET_STOCK_DATA,
+    data: data.data.cryptoCurrencyList,
+  });
 };
 
 const stockDataReducer = (state = initialState, action) => {
