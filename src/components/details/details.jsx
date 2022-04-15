5d0d380324feb536bdea047c692a5294efdc5c40
Saadat123456 <saadatlucky313@gmail.com>
2022-04-15 10:22:15 +0500

    Create Details Page

--- src/components/details/details.jsx
+++ src/components/details/details.jsx
@@ -1,5 +1,27 @@
-const Details = () => (
-  <div>Details</div>
-);
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import LoadingSpinner from '../common/loading-spinner';
+import StockDetails from './stock-details';
+
+const Details = () => {
+  const { id } = useParams();
+  const data = useSelector((state) => state.stockDataReducer);
+  const styles = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  };
+  return (
+    <div style={styles}>
+      {
+        data.length === 0 ? (
+          <LoadingSpinner />
+        ) : (
+          <StockDetails stock={data[id - 1]} />
+        )
+      }
+    </div>
+  );
+};
 
 export default Details;
